a21b1e4c0ec285cd050438517141fa2f37f791ad
adriancuadros <adrian@reservamos.mx>
2018-08-18 19:49:22 -0500

    First success

--- app/hacks.js
+++ app/hacks.js
@@ -1,1 +1,12 @@
-console.log('This is for hacking');
+import axios from 'axios';
+
+let movies = [];
+
+axios.post('http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity', {
+  claveCiudad: "monterrey-sur",
+  esVIP: false
+}).then((response) => {
+  response.data.d.Cinemas.forEach((theater) => {
+    console.log(theater.Name);
+  })
+})
