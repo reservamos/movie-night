5eaf475d002b2e52c30ffcb544378c19f8a7e3c6
adriancuadros <adrian@reservamos.mx>
2018-08-18 20:09:07 -0500

    Win 2

--- app/hacks.js
+++ app/hacks.js
@@ -2,17 +2,32 @@ import axios from 'axios';
 
 let movies = [];
 
+function includesMovie(movies, key) {
+  return movies.find((movie) => {
+    return movie.key === key
+  });
+}
+
+function extractMovies(cinemas) {
+  let movies = [];
+  cinemas.forEach((theater) => {
+    theater.Dates[0].Movies.forEach((movie) => {
+      if (!includesMovie(movies, movie.Key)) {
+        movies.push({
+          "title": movie.Title,
+          "originalTitle": movie.OriginalTitle,
+          "key": movie.Key,
+          "theaters": [theater.Name]
+        });
+      }
+    });
+  });
+  return movies
+}
+
 axios.post('http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity', {
   claveCiudad: "monterrey-sur",
   esVIP: false
 }).then((response) => {
-  response.data.d.Cinemas.forEach((theater) => {
-    theater.Dates[0].Movies.forEach((movie) => {
-      console.log({
-        "title": movie.Title,
-        "originalTitle": movie.OriginalTitle,
-        "key": movie.Key
-      })
-    });
-  })
+  console.log(extractMovies(response.data.d.Cinemas));
 })
