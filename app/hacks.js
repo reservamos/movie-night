3be5115d046fc4947a8bf4a75a758562bcbe82ab
adriancuadros <adrian@reservamos.mx>
2018-08-18 20:50:58 -0500

    Ready for reuse rotten tomatoes call :)

--- app/hacks.js
+++ app/hacks.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
 
-axios.get('https://www.rottentomatoes.com/api/private/v2.0/search/', {
-  params: {
-    limit: 1,
-    q: "Christopher Robin"
-  }
-}).then((response) => {
-  const movie = response.data.movies[0];
-  console.log({
-    rtScore: movie.meterScore,
-    rtClass: movie.meterClass
-  });
-})
+function getRottenTomatoesData(movieName) {
+  return axios.get('https://www.rottentomatoes.com/api/private/v2.0/search/', {
+    params: {
+      limit: 1,
+      q: movieName
+    }
+  }).then((response) => {
+    const movie = response.data.movies[0];
+    return {
+      rtScore: movie.meterScore,
+      rtClass: movie.meterClass
+    };
+  })
+}
+
+getRottenTomatoesData("Mission Impossible v").then((data) => {
+  console.log(data);
+});
