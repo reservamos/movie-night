ca05c76495a0662d52c063cf8d1a2926ae9e109b
adriancuadros <adrian@reservamos.mx>
2018-08-18 20:23:57 -0500

    V1 done! movies extracted

---
+++ app/cinepolis.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+function findMovieIndex(movies, key) {
+  return movies.findIndex((movie) => {
+    return movie.key === key
+  });
+}
+
+function extractMovies(apiTheaters) {
+  let movies = [];
+  apiTheaters.forEach((apiTheater) => {
+    apiTheater.Dates[0].Movies.forEach((apiMovie) => {
+      let movieIndex = findMovieIndex(movies, apiMovie.Key);
+      if (movieIndex !== -1) {
+        movies[movieIndex].theaters.push(apiTheater.Name);
+      } else {
+        movies.push({
+          "title": apiMovie.Title,
+          "originalTitle": apiMovie.OriginalTitle,
+          "key": apiMovie.Key,
+          "theaters": [apiTheater.Name]
+        });
+      }
+    });
+  });
+  return movies;
+}
+
+export function getTodaysMovies() {
+  return axios.post('http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity', {
+    claveCiudad: "monterrey-sur",
+    esVIP: false
+  }).then((response) => {
+    return extractMovies(response.data.d.Cinemas);
+  })
+}
--- app/hacks.js
+++ app/hacks.js
@@ -1,33 +1,6 @@
-import axios from 'axios';
+import { getTodaysMovies } from 'cinepolis';
 
-let movies = [];
-
-function includesMovie(movies, key) {
-  return movies.find((movie) => {
-    return movie.key === key
-  });
-}
-
-function extractMovies(cinemas) {
-  let movies = [];
-  cinemas.forEach((theater) => {
-    theater.Dates[0].Movies.forEach((movie) => {
-      if (!includesMovie(movies, movie.Key)) {
-        movies.push({
-          "title": movie.Title,
-          "originalTitle": movie.OriginalTitle,
-          "key": movie.Key,
-          "theaters": [theater.Name]
-        });
-      }
-    });
-  });
-  return movies
-}
-
-axios.post('http://www.cinepolis.com/Cartelera.aspx/GetNowPlayingByCity', {
-  claveCiudad: "monterrey-sur",
-  esVIP: false
-}).then((response) => {
-  console.log(extractMovies(response.data.d.Cinemas));
-})
+// We hacked cinepolis! 
+getTodaysMovies().then((movies) => {
+  console.log(movies);
+});
