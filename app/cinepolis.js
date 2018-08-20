a4d02679aebe6c0b48d9639e3a563281418ab076
adriancuadros <adrian@reservamos.mx>
2018-08-19 21:02:40 -0500

    Adds movie times

--- app/cinepolis.js
+++ app/cinepolis.js
@@ -6,19 +6,39 @@ const findMovieIndex = (movies, key) => {
   });
 }
 
+
+const extractTimes = (apiMovie) => {
+  let result = []
+  apiMovie.Formats.forEach((format) => {
+    let formatTimes = `${format.Language}:`;
+    format.Showtimes.forEach((showtime) => {
+      formatTimes += `${showtime.Time},`;
+    });
+    result.push(formatTimes);
+  });
+  return result;
+}
+
 const extractMovies = (apiTheaters) => {
   let movies = [];
   apiTheaters.forEach((apiTheater) => {
     apiTheater.Dates[0].Movies.forEach((apiMovie) => {
       let movieIndex = findMovieIndex(movies, apiMovie.Key);
       if (movieIndex !== -1) {
-        movies[movieIndex].theaters.push(apiTheater.Name);
+        movies[movieIndex].theaters.push({
+          name: apiTheater.Name,
+          times: extractTimes(apiMovie)
+        });
       } else {
         movies.push({
+          "image": apiMovie.Poster,
           "title": apiMovie.Title,
           "originalTitle": apiMovie.OriginalTitle,
           "key": apiMovie.Key,
-          "theaters": [apiTheater.Name]
+          "theaters": [{
+            name: apiTheater.Name,
+            times: extractTimes(apiMovie)
+          }],
         });
       }
     });
